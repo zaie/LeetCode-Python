9f2b56a299a8be6def0fb0d98234ba7975aee1bd
zaie <948064756@qq.com>
2021-05-19 22:42:26 +0800

    Update 0070-爬楼梯.py

--- 0070.爬楼梯/0070-爬楼梯.py
+++ 0070.爬楼梯/0070-爬楼梯.py
@@ -1,19 +1,13 @@
+# 倒推一下，假设当前位于第 n 阶，那么上一步可能在第 n-1 或者第 n-2 阶，分别需要爬 1 级台阶和 2 级台阶。
+# 那么，f(n) = f(n-1) + f(n-2)，
+
 class Solution(object):
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n <= 2:
-            return [1, 2][n - 1]
-        first = 1
-        second = 2
-        cnt = 2
-        while cnt < n:
-            cnt += 1
-            cur = first + second
-            if cnt == n:
-                return cur
-            first = second
-            second = cur
+    # 一维dp，自底向上
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[-1]
+
         
