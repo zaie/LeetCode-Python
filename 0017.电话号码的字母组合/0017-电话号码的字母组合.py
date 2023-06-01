b3a30cf6eec25ea5f5addb0677e077aebb7b71db
zaie <948064756@qq.com>
2023-06-01 17:47:44 +0800

    Update 0017-电话号码的字母组合.py

--- 0017.电话号码的字母组合/0017-电话号码的字母组合.py
+++ 0017.电话号码的字母组合/0017-电话号码的字母组合.py
@@ -1,22 +1,29 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
-        if not digits:
-            return []
-        mapping = {2:"abc", 3:"def", 4:"ghi", 5:"jkl", 6:"mno", 7:"pqrs", 8:"tuv", 9:"wxyz"}
-               
+当题目中出现 “所有组合” 等类似字眼时，我们第一感觉就要想到用回溯。
+
+定义函数 backtrack(combination, nextdigit)，当 nextdigit 非空时，对于 nextdigit[0] 中的每一个字母 letter，
+执行回溯 backtrack(combination + letter,nextdigit[1:]，直至 nextdigit 为空。最后将 combination 加入到结果中。
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+
+        phone = {'2':['a','b','c'],
+                 '3':['d','e','f'],
+                 '4':['g','h','i'],
+                 '5':['j','k','l'],
+                 '6':['m','n','o'],
+                 '7':['p','q','r','s'],
+                 '8':['t','u','v'],
+                 '9':['w','x','y','z']}
+                
+        def backtrack(conbination,nextdigit):
+            if len(nextdigit) == 0:
+                res.append(conbination)
+            else:
+                for letter in phone[nextdigit[0]]:
+                    backtrack(conbination + letter,nextdigit[1:])
+
         res = []
-        # for i, digit in enumerate(digits)
-        def dfs(nums, tmp):
-            if not nums:
-                res.append(tmp)
-                return
-                   
-            for char in mapping[int(nums[0])]:
-                dfs(nums[1:], tmp + char)
-                          
-        dfs(digits, "")
+        backtrack('',digits)
         return res
