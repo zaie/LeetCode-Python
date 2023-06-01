84f5004539a4b3e82cb3f47cb6270516d0eb67fc
zaie <948064756@qq.com>
2023-06-01 17:07:57 +0800

    Update 0015-三数之和.py

--- 0015.三数之和/0015-三数之和.py
+++ 0015.三数之和/0015-三数之和.py
@@ -1,33 +1,34 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+双指针法铺垫： 先将给定 nums 排序，复杂度为 O(NlogN)O(NlogN)O(NlogN)。
+双指针法思路： 固定 333 个指针中最左（最小）数字的指针 k，双指针 i，j 分设在数组索引 (k,len(nums))(k, len(nums))(k,len(nums)) 两端，通过双指针交替向中间移动，记录对于每个固定指针 k 的所有满足 nums[k] + nums[i] + nums[j] == 0 的 i,j 组合：
+当 nums[k] > 0 时直接break跳出：因为 nums[j] >= nums[i] >= nums[k] > 0，即 333 个数字都大于 000 ，在此固定指针 k 之后不可能再找到结果了。
+当 k > 0且nums[k] == nums[k - 1]时即跳过此元素nums[k]：因为已经将 nums[k - 1] 的所有组合加入到结果中，本次双指针搜索只会得到重复组合。
+i，j 分设在数组索引 (k,len(nums))(k, len(nums))(k,len(nums)) 两端，当i < j时循环计算s = nums[k] + nums[i] + nums[j]，并按照以下规则执行双指针移动：
+当s < 0时，i += 1并跳过所有重复的nums[i]；
+当s > 0时，j -= 1并跳过所有重复的nums[j]；
+当s == 0时，记录组合[k, i, j]至res，执行i += 1和j -= 1并跳过所有重复的nums[i]和nums[j]，防止记录到重复组合。
+
+
+class Solution:
+    def threeSum(self, nums: [int]) -> [[int]]:
         nums.sort()
-        res = []
-        for i, num in enumerate(nums):
-            if i == 0 or nums[i] > nums[i - 1]:
-                # num  + b + c == 0
-                left = i + 1
-                right = len(nums) - 1
-                
-                while(left < right):
-                    s = num + nums[left] + nums[right]
-                    if s == 0:
-                        res.append([num, nums[left], nums[right]])
-                        left += 1
-                        right -= 1
-                        while(left < right and nums[left] == nums[left - 1]):
-                            left += 1
-                        while(left < right and nums[right + 1] == nums[right]):
-                            right -= 1
-                        # break
-                    elif s > 0:
-                        right -= 1
-                    else:
-                        left += 1
-                
-                    
+        res, k = [], 0
+        for k in range(len(nums) - 2):
+            if nums[k] > 0: break # 1. because of j > i > k.
+            if k > 0 and nums[k] == nums[k - 1]: continue # 2. skip the same `nums[k]`.
+            i, j = k + 1, len(nums) - 1
+            while i < j: # 3. double pointer
+                s = nums[k] + nums[i] + nums[j]
+                if s < 0:
+                    i += 1
+                    while i < j and nums[i] == nums[i - 1]: i += 1
+                elif s > 0:
+                    j -= 1
+                    while i < j and nums[j] == nums[j + 1]: j -= 1
+                else:
+                    res.append([nums[k], nums[i], nums[j]])
+                    i += 1
+                    j -= 1
+                    while i < j and nums[i] == nums[i - 1]: i += 1
+                    while i < j and nums[j] == nums[j + 1]: j -= 1
         return res
                     
