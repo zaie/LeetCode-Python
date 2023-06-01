566c5dfa1e40ad28d57a1d143c24fb8da457f68d
zaie <948064756@qq.com>
2023-06-01 10:11:10 +0800

    Update 0003-无重复字符的最长子串.py

--- 0003.无重复字符的最长子串/0003-无重复字符的最长子串.py
+++ 0003.无重复字符的最长子串/0003-无重复字符的最长子串.py
@@ -1,16 +1,19 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        left, right = 0, 0
-        dic = dict()
+# 双指针，如果有重复的就动左指针，否则就动右指针，记录最大长度
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left,right = 0,0
         res = 0
+        if len(s) == 0:
+            return 0
+        if s.count(s[0]) == len(s):
+            return 1
+        if len(set(s)) == len(s):
+            return len(s)
         while right < len(s):
-            if s[right] in dic:
-                left = max(left, dic[s[right]] + 1)
-            dic[s[right]] = right
-            res = max(res, right - left + 1)
-            right += 1
+            if s[right] not in s[left:right]:
+                right +=1
+                res = max(res,len(s[left:right]))
+            else:
+                while s[right] in s[left:right]:
+                    left +=1
         return res
