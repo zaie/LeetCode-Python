2be044b9d1a963449c8654f8edf12d9758067a93
zaie <948064756@qq.com>
2023-06-01 11:02:33 +0800

    Update 0004-寻找两个有序数组的中位数.py

--- 0004.寻找两个有序数组的中位数/0004-寻找两个有序数组的中位数.py
+++ 0004.寻找两个有序数组的中位数/0004-寻找两个有序数组的中位数.py
@@ -1,35 +1,19 @@
-from heapq import *
-class DoubleHeap(object):
-    def __init__(self):
-        self.maxh = []
-        self.minh = []
-        heapify(self.maxh)
-        heapify(self.minh)
-        
-    def insert(self, val):
-        heappush(self.minh, val)
-        heappush(self.maxh, -heappop(self.minh))
-        if len(self.maxh) > len(self.minh):
-            heappush(self.minh, -heappop(self.maxh))
-            
-    def findMedian(self):
-        if len(self.maxh) == len(self.minh):
-            return (self.minh[0] - self.maxh[0]) / 2.0
-        return self.minh[0]/1.0
+# 假设 nums1[i-1]<=nums2[j] 且 nums2[j-1]<=nums1[i]，nums1[:i] 和 nums2[:j] 就是最小的 i+j 个数
+# 当 i+j==(m+n)//2 时，即找到了最小的一半数，即可求出中位数
+# 因此遍历和为 (m+n)//2 的 <i, j> 对，找到满足 nums1[i-1]<=nums2[j] 且 nums2[j-1]<=nums1[i] 的即可
+# 注意 i 递增时，nums1[i-1]、nums1[i] 递增，nums2[j]、nums2[j-1] 递减，因此遍历找到第一个满足 nums2[j-1]<=nums1[i] 的即可。
 
+# 为了方便，当 m>n 时，可以交换 nums1、nums2 使得 m<=n，排除边界情况。
 
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
-        dh = DoubleHeap()
-        for num in nums1:
-            dh.insert(num)
-            
-        for num in nums2:
-            dh.insert(num)
-            
-        return dh.findMedian()
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        m, n = len(nums1), len(nums2)
+        if m > n:
+            return self.findMedianSortedArrays(nums2, nums1)
+        i, j = 0, (m+n) // 2
+        while i < m and nums1[i] < nums2[j-1]:
+            i += 1
+            j -= 1
+        left = max(nums1[i-1] if i else float('-inf'), nums2[j-1] if j else float('-inf'))
+        right = min(nums1[i] if i<m else float('inf'), nums2[j]if j<n else float('inf'))
+        return right if (m + n) % 2 else (left+right) / 2
