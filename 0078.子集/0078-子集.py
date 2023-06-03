6b7c41ce1c937096cb6da8753eb3d6e893a72528
zaie <948064756@qq.com>
2023-06-03 18:07:31 +0800

    Update 0078-子集.py

--- 0078.子集/0078-子集.py
+++ 0078.子集/0078-子集.py
@@ -1,14 +1,23 @@
-class Solution(object):
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        res = [[]]
-        for num in nums:
-            tmp = res[:]
-            for item in res:
-                tmp.append(item + [num])     
-            res = tmp[:]
-            
+# 思路一、库函数
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        for i in range(len(nums)+1):
+            for tmp in itertools.combinations(nums, i):
+                res.append(tmp)
         return res
+    
+# 思路二、迭代
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for i in nums:
+            res = res + [[i] + num for num in res]
+        return res
+
+# 作者：powcai
+# 链接：https://leetcode.cn/problems/subsets/solutions/6899/hui-su-suan-fa-by-powcai-5/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
