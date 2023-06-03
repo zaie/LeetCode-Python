b3bff83a3e3d9365f7b2f40f1fe2f9646ebf18a1
zaie <948064756@qq.com>
2023-06-03 20:27:55 +0800

    Update 0121-买卖股票的最佳时机.py

--- 0121.买卖股票的最佳时机/0121-买卖股票的最佳时机.py
+++ 0121.买卖股票的最佳时机/0121-买卖股票的最佳时机.py
@@ -1,12 +1,14 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        res = 0
-        pre_min = prices[0] if prices else 0
+# 遍历一遍数组，计算每次 到当天为止 的最小股票价格和最大利润。
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minprice = float('inf')
+        maxprofit = 0
         for price in prices:
-            res = max(res, price - pre_min)
-            pre_min = min(pre_min, price)
-        return res
+            minprice = min(minprice, price)
+            maxprofit = max(maxprofit, price - minprice)
+        return maxprofit
+
+# 作者：腐烂的橘子
+# 链接：https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/solutions/139559/gu-piao-wen-ti-python3-c-by-z1m/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
