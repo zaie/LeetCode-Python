e46da2c677f8e076a35030f2c84da21a8c2eab75
zaie <948064756@qq.com>
2023-06-05 11:28:11 +0800

    Update 0207-课程表.py

--- 0207.课程表/0207-课程表.py
+++ 0207.课程表/0207-课程表.py
@@ -1,29 +1,65 @@
+这道题 就是找图中是否有环。
+
+思路一：深度优先遍历
+时间复杂度：O(N+E) N 为顶点个数，E为边的个数
+
+思路二：广度优先遍历
+通过顶点的入度的个数，每次消除入度为0顶点，看是否每一个节点都能被消除
+
+时间复杂度 ： O(N+E) N为顶点个数，E为边的个数
+
+DFS
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # 1. find all node/course with indegree 0, let them enter a queue
+        from collections import defaultdict
+        graph = defaultdict(list)
+        # 记录
+        visited = set()
+        # 建图
+        for x, y in prerequisites:
+            graph[y].append(x)
+
+        # 深度遍历
+        def dfs(i, being_visited):
+            if i in being_visited: return False
+            if i in visited: return True
+            visited.add(i)
+            being_visited.add(i)
+            for j in graph[i]:
+                if not dfs(j, being_visited): return False
+            being_visited.remove(i)
+            return True
+        # 检测每门功课起始是否存在环
+        for i in range(numCourses):
+            # 已经访问过
+            if i in visited: continue
+            if not dfs(i, set()): return False
+        return True
+    
+BFS
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         from collections import defaultdict, deque
-        indegree = defaultdict(int)
-        children = defaultdict(set)
-        all_courses = set()
-        for cur, pre in prerequisites:
-            indegree[cur] += 1
-            children[pre].add(cur)
-            all_courses.add(cur)
-            all_courses.add(pre)
-
-        queue = deque([])
-        for course in all_courses:
-            if indegree[course] == 0:
-                queue.append(course)
-        # 2. BFS, let course with indegree 0 leave a queue, and let its children with indegree 0 into the queue
-        studied_course = 0
+        graph = defaultdict(list)
+        degree = [0] * numCourses
+        # 建图
+        for x, y in prerequisites:
+            graph[y].append(x)
+            degree[x] += 1
+        queue = deque([i for i in range(numCourses) if degree[i] == 0])
+        #print(queue)
+        cnt = 0
         while queue:
-            cur = queue.popleft()
+            i = queue.pop()
+            cnt += 1
+            for j in graph[i]:
+                degree[j] -= 1
+                if degree[j] == 0:
+                    queue.appendleft(j)
+        return cnt == numCourses
 
-            studied_course += 1
-            for child in children[cur]:
-                indegree[child] -= 1
-                if indegree[child] == 0:
-                    queue.append(child)
 
-        return studied_course == len(all_courses)
+作者：powcai
+链接：https://leetcode.cn/problems/course-schedule/solutions/21166/dfs-bfs-by-powcai/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
