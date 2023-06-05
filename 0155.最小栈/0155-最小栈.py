1eca0405c679022d94145a1427515edf96bc5887
zaie <948064756@qq.com>
2023-06-05 10:53:23 +0800

    Update 0155-最小栈.py

--- 0155.最小栈/0155-最小栈.py
+++ 0155.最小栈/0155-最小栈.py
@@ -1,43 +1,20 @@
-class MinStack(object):
-
-    def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.s = []
-        self.min_s = []
-
-    def push(self, x):
-        """
-        :type x: int
-        :rtype: None
-        """
-        self.s.append(x)
-        if self.min_s:
-            self.min_s.append(min(x, self.min_s[-1]))
-        else:
-            self.min_s.append(x)
-
-    def pop(self):
-        """
-        :rtype: None
-        """
-        self.min_s.pop()
-        self.s.pop()
-
-
-    def top(self):
-        """
-        :rtype: int
-        """
-        return self.s[-1]
-
-    def getMin(self):
-        """
-        :rtype: int
-        """
-        return self.min_s[-1]
+class MinStack:
     
+    def __init__(self):
+        self.data = [(None, float('inf'))]
+
+    def push(self, x: 'int') -> 'None':
+        self.data.append((x, min(x, self.data[-1][1])))
+
+    def pop(self) -> 'None':
+        if len(self.data) > 1: self.data.pop()
+
+    def top(self) -> 'int':
+        return self.data[-1][0]
+
+    def getMin(self) -> 'int':
+        return self.data[-1][1]
+
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
@@ -45,3 +22,8 @@ class MinStack(object):
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+# 作者：
+# 链接：https://leetcode.cn/problems/min-stack/solutions/4114/python-mei-ge-yi-xing-by-knifezhu/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
