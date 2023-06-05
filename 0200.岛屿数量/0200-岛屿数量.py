b24ba5acd1271924dfa18723d892177b0d898b73
zaie <948064756@qq.com>
2023-06-05 11:15:10 +0800

    Update 0200-岛屿数量.py

--- 0200.岛屿数量/0200-岛屿数量.py
+++ 0200.岛屿数量/0200-岛屿数量.py
@@ -1,31 +1,27 @@
+核心思想：BFS，如果发现一个陆地，便对其进行 BFS，并将可由 BFS 访问到的点都置为已经访问状态，从而代表同属一个岛屿，详解见注释。
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        from collections import deque
-        if not grid or not grid[0]:
-            return 0
+        count = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == '1':  # 发现陆地
+                    count += 1  # 结果加1
+                    grid[row][col] = '0'  # 将其转为 ‘0’ 代表已经访问过
+                    # 对发现的陆地进行扩张即执行 BFS，将与其相邻的陆地都标记为已访问
+                    # 下面还是经典的 BFS 模板
+                    land_positions = collections.deque()
+                    land_positions.append([row, col])
+                    while len(land_positions) > 0:
+                        x, y = land_positions.popleft()
+                        for new_x, new_y in [[x, y + 1], [x, y - 1], [x + 1, y], [x - 1, y]]:  # 进行四个方向的扩张
+                            # 判断有效性
+                            if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]) and grid[new_x][new_y] == '1':
+                                grid[new_x][new_y] = '0'  # 因为可由 BFS 访问到，代表同属一块岛，将其置 ‘0’ 代表已访问过
+                                land_positions.append([new_x, new_y])
+        return count
 
-        m, n = len(grid), len(grid[0])
-
-        dx = [1, -1, 0, 0]
-        dy = [0, 0, 1, -1]
-
-        res = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
-                    grid[i][j] = "0"
-                    res += 1
-                    
-                    queue = deque([(i, j)])
-
-                    while queue:
-                        x0, y0 = queue.popleft()
-                        for k in range(4):
-                            x = x0 + dx[k]
-                            y = y0 + dy[k]
-
-                            if 0 <= x < m and 0 <= y < n and grid[x][y] == "1":
-                                grid[x][y] = "0"
-                                queue.append((x, y))
-
-        return res
+作者：莲子熊猫
+链接：https://leetcode.cn/problems/number-of-islands/solutions/210855/xiong-mao-shua-ti-python3-yi-jiu-shi-bfsmo-ban-ti-/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
